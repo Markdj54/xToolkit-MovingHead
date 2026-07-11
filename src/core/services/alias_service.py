6d39819a998a596fcc519d25277372b674bfecb2
Mark Farrenden <ozzieflight@gmail.com>
2026-07-12 07:54:19 +1000

    Commit 3 - Added Alias Service

---
+++ src/core/services/alias_service.py
@@ -0,0 +1,9 @@
+load_defaults()
+
+lookup()
+
+add_alias()
+
+has_alias()
+
+count()
---
+++ src/core/services/tests/test_alias_service.py
@@ -0,0 +1,5 @@
+assert alias.lookup("Pan") == PAN
+
+assert alias.lookup("Colour") == COLOR
+
+assert alias.lookup("Color") == COLOR
