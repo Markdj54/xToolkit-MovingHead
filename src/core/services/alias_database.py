bfafccb1c7d2883bcae2a0aa4851bbec17c7cc14
Mark Farrenden <ozzieflight@gmail.com>
2026-07-13 09:26:24 +1000

    v0.5.0 - Canonical Lighting Foundation

---
+++ src/core/services/alias_database.py
@@ -0,0 +1,55 @@
+{
+    "PAN": [
+        "Pan",
+        "Pan / X-axis / Horizontal"
+    ],
+
+    "PAN_FINE": [
+        "Pan Fine"
+    ],
+
+    "TILT": [
+        "Tilt",
+        "Tilt / Y-axis / Vertical"
+    ],
+
+    "TILT_FINE": [
+        "Tilt Fine"
+    ],
+
+    "PAN_TILT_SPEED": [
+        "Pan & Tilt Speed"
+    ],
+
+    "DIMMER": [
+        "Dimmer",
+        "Intensity",
+        "Dimmer / Intensity"
+    ],
+
+    "SHUTTER": [
+        "Shutter",
+        "Strobe",
+        "Shutter / Strobe"
+    ],
+
+    "COLOR": [
+        "Color",
+        "Colour",
+        "Color Wheel"
+    ],
+
+    "PRISM": [
+        "Prism",
+        "Prism 1"
+    ],
+
+    "PRISM_ROTATE": [
+        "Prism Rotate",
+        "Prism Rotate 1"
+    ],
+
+    "UNUSED": [
+        "Blank"
+    ]
+}
